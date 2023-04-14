28aa21d677dee35fdc0eac62ab8740cfe7a3e849
wzrwzr23 <zhuoran_wang@mymail.sutd.edu.sg>
2023-04-14 15:33:39 +0800

    Data Loading

--- student_files/q1.py
+++ student_files/q1.py
@@ -9,3 +9,5 @@ hdfs_nn = sys.argv[1]
 spark = SparkSession.builder.appName("Assigment 2 Question 1").getOrCreate()
 # YOUR CODE GOES BELOW
 
+df=spark.read.format("csv").option("header","true").load("hdfs://localhost:9000/assignment2/part1/input/TA_restaurants_curated_cleaned.csv")
+df.printSchema()
