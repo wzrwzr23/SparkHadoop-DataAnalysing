4c3e82d92a57746cb4fd334b015daac040690a9a
wzrwzr23 <zhuoran_wang@mymail.sutd.edu.sg>
2023-04-16 04:09:12 +0800

    q3

--- student_files/q3.py
+++ student_files/q3.py
@@ -1,6 +1,6 @@
 import sys
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, max, min, split, count
+from pyspark.sql.functions import col, split, arrays_zip, explode, regexp_replace
 
 # you may add more import if you need to
 
@@ -12,6 +12,23 @@ spark = SparkSession.builder.appName("Assigment 2 Question 3").getOrCreate()
 # YOUR CODE GOES BELOW
 df = spark.read.option("header",True).csv("hdfs://%s:9000/assignment2/part1/input/" % (hdfs_nn))
 
-rev = split(df["Reviews"], '\\], \\[')
-rev_df = df.withColumn("review", rev.getItem(0).withColumn("date", rev.getItem(1)))
+df = df.select(df.ID_TA, df.Reviews)
+
+reviews = split(df.Reviews, '\\], \\[')
+rev_df = df.withColumn("rev", reviews.getItem(0))\
+        .withColumn("dat", reviews.getItem(1))\
+        .withColumn("review", split(col("rev"), "\\', \\'"))\
+        .withColumn("date", split(col("dat"), "\\', \\'"))\
+        .drop(col("rev")).drop(col("dat"))\
+        .withColumn("combined", arrays_zip("review", "date"))\
+        .withColumn("combined", explode("combined"))\
+        .withColumn("review", col("combined.review"))\
+        .withColumn("date", col("combined.date"))\
+        .drop(col("Reviews")).drop(col("combined"))\
+        .withColumn("review", regexp_replace("review", "\\[", ""))\
+        .withColumn("date", regexp_replace("date", "\\]", ""))\
+        .withColumn("review", regexp_replace("review", "'", ""))\
+        .withColumn("date", regexp_replace("date", "'", ""))\
+        .withColumn("date", regexp_replace("date", " ", ""))
 rev_df.show()
+rev_df.write.csv("hdfs://%s:9000/assignment2/output/question1/" % (hdfs_nn), header=True)
