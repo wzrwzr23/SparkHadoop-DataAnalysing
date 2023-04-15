16042ae2343fe84854839d82f8a691bf9433bb8c
wzrwzr23 <zhuoran_wang@mymail.sutd.edu.sg>
2023-04-15 21:20:34 +0800

    q3

--- student_files/q3.py
+++ student_files/q3.py
@@ -1,5 +1,6 @@
 import sys
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, max, min, split, count
 
 # you may add more import if you need to
 
@@ -9,3 +10,6 @@ hdfs_nn = sys.argv[1]
 
 spark = SparkSession.builder.appName("Assigment 2 Question 3").getOrCreate()
 # YOUR CODE GOES BELOW
+df = spark.read.option("header",True).csv("hdfs://%s:9000/assignment2/part1/input/" % (hdfs_nn))
+review = df.split(col("Review"), '\\], \\[')
+review.show()
