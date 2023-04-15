d37dfac063b64dc5f634876eb62eb7499f4e1997
wzrwzr23 <zhuoran_wang@mymail.sutd.edu.sg>
2023-04-16 04:15:46 +0800

    q4

--- student_files/q4.py
+++ student_files/q4.py
@@ -1,5 +1,6 @@
 import sys
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, split, arrays_zip, explode, regexp_replace
 
 # you may add more import if you need to
 
@@ -9,3 +10,6 @@ hdfs_nn = sys.argv[1]
 
 spark = SparkSession.builder.appName("Assigment 2 Question 4").getOrCreate()
 # YOUR CODE GOES BELOW
+df = spark.read.option("header",True).csv("hdfs://%s:9000/assignment2/part1/input/" % (hdfs_nn))
+df = df.select(col("Name"), col("City"), col("Cuisine Style"))
+df.show()
